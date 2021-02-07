f01ab663bcbb017ce0e79d541b36e65c19f13f02
Adobeosu <61052022+Adobeosu@users.noreply.github.com>
2021-02-07 02:26:14 -0500

    short = better in my book

--- utils.py
+++ utils.py
@@ -1,30 +1,8 @@
 import os
 
-BLACK = "\033[0;30m"
-RED = "\033[0;31m"
-GREEN = "\033[0;32m"
-BROWN = "\033[0;33m"
-BLUE = "\033[0;34m"
-PURPLE = "\033[0;35m"
-CYAN = "\033[0;36m"
-LIGHT_GRAY = "\033[0;37m"
-DARK_GRAY = "\033[1;30m"
-LIGHT_RED = "\033[1;31m"
-LIGHT_GREEN = "\033[1;32m"
-YELLOW = "\033[1;33m"
-LIGHT_BLUE = "\033[1;34m"
-LIGHT_PURPLE = "\033[1;35m"
-LIGHT_CYAN = "\033[1;36m"
-LIGHT_WHITE = "\033[1;37m"
-BOLD = "\033[1m"
-FAINT = "\033[2m"
-ITALIC = "\033[3m"
-UNDERLINE = "\033[4m"
-BLINK = "\033[5m"
-NEGATIVE = "\033[7m"
-CROSSED = "\033[9m"
-ENDC = '\033[0m'
-PINK = '\033[95m'
+BLACK, RED, GREEN, BROWN, BLUE, PURPLE = "\033[0;30m", "\033[0;31m", "\033[0;32m", "\033[0;33m", "\033[0;34m", "\033[0;35m"
+CYAN, LIGHT_GRAY, DARK_GRAY, LIGHT_RED, LIGHT_GREEN, YELLOW = "\033[0;36m", "\033[0;37m", "\033[1;30m", "\033[1;31m", "\033[1;32m", "\033[1;33m"
+LIGHT_BLUE, LIGHT_PURPLE, LIGHT_CYAN, LIGHT_WHITE, ENDC, PINK = "\033[1;34m", "\033[1;35m", "\033[1;36m", "\033[1;37m", '\033[0m', '\033[95m'
 
 def printConsole():
 	os.system('cls' if os.name=='nt' else 'clear')
