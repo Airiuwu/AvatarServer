62ebd2e840838e312e20f8219a9ebab49d291201
Adobeosu <61052022+Adobeosu@users.noreply.github.com>
2021-02-08 04:19:17 -0500

    faster this way, dont ask

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import uvicorn, utils, handlers
+import os, uvicorn
 from starlette.applications import Starlette
 from starlette.routing import Route
+from starlette.responses import FileResponse
 
-app = Starlette(routes=[Route('/', endpoint=handlers.homepage, methods=['GET']),Route('/{id:int}', endpoint=handlers.avatarRequest, methods=['GET'])])
+async def avatarRequest(request):
+	uid = request.path_params['id']
 
-utils.printConsole()
+	if os.path.isfile("avatars/{}.png".format(uid)):
+		return FileResponse("avatars/{}.png".format(uid))
+
+	return FileResponse("avatars/-1.png")
+
+async def homepage(request):
+	return FileResponse("avatars/-1.png")
+
+app = Starlette(routes=[
+    Route('/', endpoint=homepage, methods=['GET']),
+    Route('/{id:int}', endpoint=avatarRequest, methods=['GET'])
+])
 
 uvicorn.run(app, host="127.0.0.1", port=5000)
