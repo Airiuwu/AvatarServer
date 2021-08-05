bb54de5097d4857a60b6cc1e6662c24739d8faf9
Airi <61052022+Airiuwu@users.noreply.github.com>
2021-08-05 15:16:25 -0400

    uniformity.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import os, random
+import os
+from random import randint as randomNumber
 from quart import Quart, send_file
 
-app, defaultAvatars = Quart(__name__), ["-1", "-2", "-3", "-4", "-5"]
+app = Quart(__name__)
 
 @app.route("/")
 async def homepage():
-	return await send_file("avatars/{}.png".format(random.choice(defaultAvatars)))
+	return await send_file(f"avatars/-{randomNumber(1, 5)}.png")
 
 @app.route("/<int:uid>")
 async def avatarRequest(uid):
 	if os.path.exists(f"avatars/{uid}.png"):
 		return await send_file(f"avatars/{uid}.png")
 
-	return await send_file("avatars/{}.png".format(random.choice(defaultAvatars)))
+	return await send_file(f"avatars/-{randomNumber(1, 5)}.png")
 
 app.run()
